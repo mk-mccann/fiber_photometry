3e7b06b5ac039585e1977fd8012c3b03e6c55df1
matt m <matthew.mccann13@gmail.com>
2021-11-11 16:04:54 +0100

    Across animal HMM

--- hmm_episodes.py
+++ hmm_episodes.py
@@ -5,32 +5,76 @@ from hmmlearn import hmm
 from functions_io import load_all_experiments
 
 
+def fit_hmm_single_animal_day(df, animal, day, key='behavior'):
+    sub_df = df.loc[(df.animal == str(animal)) & (df.day == str(day))]
+    data = sub_df[key].to_list()
+
+    collapsed_data = collapse_samples_to_states(data)
+    collapsed_data = [list(filter(None, s)) for s in collapsed_data]
+    flattened_data = np.concatenate(collapsed_data)
+
+    unique_labels = np.unique(flattened_data)
+    integer_data = np.array([np.argwhere(unique_labels == x)[0][0] for x in flattened_data])
+
+    remodel = hmm.MultinomialHMM(n_components=len(unique_labels), covariance_type='full',
+                                 n_iter=100, tol=0.01,
+                                 verbose=True)
+
+    X = integer_data.reshape(-1, 1)
+
+    return remodel, X, unique_labels
+
+
+def collapse_samples_to_states(data):
+    collapsed_data = []
+    for row in data:
+        collapsed = [x for x, y in zip(row[:-1], row[1:]) if x != y]
+        collapsed.append(row[-1])
+        collapsed_data.append(collapsed)
+    return collapsed_data
+
+
+def fit_hmm_across_animals(df, day, key='behavior'):
+    sub_df = df.loc[df.day == str(day)]
+    per_animal_data = sub_df.groupby(['animal'])[key].agg(list)
+
+    # Collapse blocks of samples in the same state to a single observation and
+    # remove unlabeled states
+    collapsed_data = collapse_samples_to_states(per_animal_data)
+    collapsed_data = [list(filter(None, s))for s in collapsed_data]
+    flattened_data = np.concatenate(collapsed_data)
+    lengths = [len(s) for s in collapsed_data]
+
+    unique_labels = np.unique(flattened_data)
+    integer_data = np.array([np.argwhere(unique_labels == x)[0][0] for x in flattened_data])
+
+    X = integer_data.reshape(-1, 1)
+
+    return X, lengths, unique_labels
+
+
 if __name__ == "__main__":
-    animal = 2
-    day = 1
 
     all_data = load_all_experiments()
-    subset = all_data.loc[(all_data.animal == str(animal)) & (all_data.day == str(day))]
+    labeled_episodes = all_data.columns[all_data.columns.get_loc('zone') + 1:]
 
-    # make the zones and behavior
-    labeled_episodes = subset.columns[subset.columns.get_loc('zone') + 1:]
+    # Get labeled behaviors, but exclude Eating and Transfer
+    behaviors_to_exclude = ['Eating', 'Transfer', 'Switch', 'WSW', 'Ear Scratch', 'Nibbling Floor', 'Squeezed MZ Edge']
     found_behaviors = [ep for ep in labeled_episodes if 'Zone' not in ep]
-    behaviors_to_use = [fb for fb in found_behaviors if fb != 'Eating']
-    behaviors = subset[behaviors_to_use].fillna('').sum(axis=1).to_numpy()
-    unique_behaviors = np.unique(behaviors)
-    behaviors = np.array([np.argwhere(unique_behaviors == x)[0][0] for x in behaviors])
-    behaviors = behaviors[behaviors > 0]
+    behaviors_to_use = [fb for fb in found_behaviors if fb not in behaviors_to_exclude]
 
-    # behaviors = subset.behavior.to_numpy
-    zones = subset.zone.to_numpy()
-    unique_zones = np.unique(zones)
-    zones = np.array([np.argwhere(unique_zones == x)[0][0] for x in zones])
+    # Overwrite the behavior column with the behaviors we are interested in
+    all_data['behavior'] = all_data[behaviors_to_use].fillna('').sum(axis=1)
 
-    remodel = hmm.GaussianHMM(n_components=len(np.unique(behaviors)), covariance_type='full',
-                              n_iter=100, tol=0.01,
-                              verbose=True)
-    remodel.fit(behaviors.reshape(-1, 1))
-    remodel.predict(behaviors.reshape(-1, 1))
-    remodel.decode(behaviors.reshape(-1, 1))
+    X, lengths, states = fit_hmm_across_animals(all_data, 1, key='behavior')
+
+    remodel = hmm.MultinomialHMM(n_components=len(states),
+                                 n_iter=100, tol=0.01,
+                                 verbose=True)
+
+    Z2 = remodel.fit(X, lengths)
+
+    # hmm_animal_day, X, states = fit_hmm_single_animal_day(all_data, animal, day)
+    # Z3 = hmm_animal_day.fit(X)
 
     print('hi')
